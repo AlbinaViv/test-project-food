35675dbbb1bc8142a7c80414aacda225b857519e
Albina Vivdenko <albinka3322@gmail.com>
2024-01-04 17:38:22 +0100

    css header

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,18 +1,70 @@
 import axios from "axios";
-
- async function fetchProducts() {
-    try { 
-        const { data } = await axios.get("https://food-boutique.b.goit.study/api/products");
-     return data
+import { addLoader, removeLoader } from "./loader";
+// axios.defaults.baseURL = "https://food-boutique.b.goit.study/api/"; приклад використання, але другий більш універсальний
+const axiosGet = axios.create({
+    baseURL: "https://food-boutique.b.goit.study/api/",
+})
+async function fetchProducts(url) {
+    try {
+        // addLoader();
+        const { data } = await axiosGet(url);
+        return data
         
     } catch (error) {
         console.log(error);
-    } 
+    } finally { 
+        // removeLoader();
+    }
  }
-// fetchProducts().then((products) => {
-//     return products;
-// }).then((products) => (products)).then(console.log) є три варінти повернення даних з ф-ц. Нижче я обрала найзручніший варіант(.then)
+function getAllProducts() {
+    const url = "/products";
+    return fetchProducts(url);
+    
+}
+// getAllProducts().then(console.log)
 
-fetchProducts().then((products) => {
-    return products;
-})
+// return fetchProducts(url/popular);
+
+function getPopularProducts() {
+    const url = "/products/popular";
+
+    return fetchProducts(url);
+    
+    
+}
+getPopularProducts().then(console.log)
+
+function getDiscountProducts() {
+    const url = "/products/discount";
+
+    return fetchProducts(url);
+    
+    
+}
+getDiscountProducts().then(console.log)
+
+// async function getCategories() {
+//     const url = "/products/categories";
+//     const data = await fetchProducts(url);
+//     const categories = data.map((category) => {
+//         const categoryArr = category.split('_');
+//         const newCategory = categoryArr.join(' ');
+//         return newCategory;
+//     });
+//     return categories;
+// } повний запис, а я використовую короткий ->
+
+async function getCategories() {
+    const url = "/products/categories";
+    const data = await fetchProducts(url);
+    return data.map((category) => category.split('_').join(' '))
+};
+    
+getCategories().then(console.log)
+
+
+function getProduct(id) {
+    const url = `/products/${id}`;
+    return fetchProducts(url);
+}
+// getProduct("640c2dd963a319ea671e3860").then(console.log)
---
+++ src/js/loader.js
@@ -0,0 +1,13 @@
+import { Loading } from 'notiflix';
+
+export function addLoader() {
+    Loading.pulse({
+        backgroundColor: "rgba(0,0,0,0.8)",
+        svgSize: "70px",
+        svgColor: "var(--primary-dark-color)",
+    });
+};
+
+export function removeLoader() {
+    Loading.remove(2000);
+};
