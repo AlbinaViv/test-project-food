589b3345cccb23eb864d5a206bf2d3d58b5f50e7
Albina Vivdenko <albinka3322@gmail.com>
2024-01-05 21:16:30 +0100

    css cards, class markup

--- src/js/cards.js
+++ src/js/cards.js
@@ -2,14 +2,14 @@ import { getAllProducts } from "./fetch";
 
 export async function mainCardsMarkup(arr) {
     const {results} = await getAllProducts();
-     const markup = results.map(({img, name, category, popularity, price, size, _id , is10PercentOff})=>`<li>
-     <a href=""><img src="${img}" alt="${name}">
-         <div class="description">
-             <h3>${name}</h3>
+     const markup = results.map(({img, name, category, popularity, price, size, _id , is10PercentOff})=>`<li class="cards-products-list">
+     <a href=""><div class="wrap-img"><img class="cards-img" src="${img}" alt="${name}"></div>
+     <div class="description">
+             <h3 class="descr-title">${name}</h3>
              <ul>
-                 <li><span>${category}</span></li>
-                 <li><span>${size}</span></li>
-                 <li><span>${popularity}</span></li>
+                 <li class="cards-text"><span>${category}</span></li>
+                 <li class="cards-text"><span>${size}</span></li>
+                 <li class="cards-text"><span>${popularity}</span></li>
              </ul>
          </div>
          <div class="price">
@@ -24,6 +24,7 @@ export async function mainCardsMarkup(arr) {
     const ul =document.createElement('ul');
     ul.innerHTML = markup;
     // створили ul і всередину(innerHTML) поклали markup 
+    ul.classList = "list cards-list";
     return ul;
 
 };
