db01c1480cd1e82bcf387ebc2ad20d4dd1a2ee29
Albina Vivdenko <albinka3322@gmail.com>
2024-01-09 11:31:47 +0100

    1

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,13 @@
 import { Loading } from 'notiflix';
 
 export function addLoader() {
-    Loading.pulse({
-        backgroundColor: "rgba(0,0,0,0.8)",
-        svgSize: "70px",
-        svgColor: "var(--primary-dark-color)",
-    });
-};
+  Loading.pulse({
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    svgSize: '70px',
+    svgColor: 'var(--primary-dark-color)',
+  });
+}
 
 export function removeLoader() {
-    Loading.remove(2000);
-};
+  Loading.remove(2000);
+}
